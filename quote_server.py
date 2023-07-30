945d68d40f54ee6abc3f1a902640babddbf5c5fa
Nathan <n8nyoung@gmail.com>
2023-07-29 22:06:58 -0600

    Moved quote database into backend object

--- quote_server.py
+++ quote_server.py
@@ -9,35 +9,9 @@ import quote_updater as quote_db
 from server import HTTPHandler 
 from utils import merge_dict_into_dict, dict_keys_to_lowercase
 
-CURRENT_QUOTE_INDEX = 0
 
-def get_current_quote(quotes):
-
-    # TODO, nathan im sure youre gonna rewrite this so im gonna do the bare minimum here
-
-    global CURRENT_QUOTE_INDEX
-
-    n_quotes = len(quotes)
-
-    chosen_quote = None
-
-    # safety check db size
-    if n_quotes > 0:
-        chosen_quote = quotes.iloc[CURRENT_QUOTE_INDEX % n_quotes].to_dict()
-    
-    # increment index
-    # CURRENT_QUOTE_INDEX = CURRENT_QUOTE_INDEX+1
-
-    return chosen_quote
-
-
-def change_current_quote():
-    global CURRENT_QUOTE_INDEX
-    CURRENT_QUOTE_INDEX = CURRENT_QUOTE_INDEX+1
-
-
-# fetch the new quotes file and save it if it is different than what we already have in memory
 def fetch_and_save_database(source, sheet, cache_file, existing_db=None, cleaning_func=None):
+    # generic fetch and save function for both quotes and birthday information
     new_db_raw = quote_db.get_spreadsheet(source, sheet)
     new_db = cleaning_func(new_db_raw) if cleaning_func else new_db_raw
     if (existing_db is None) or (not existing_db.equals(new_db)):
@@ -45,21 +19,8 @@ def fetch_and_save_database(source, sheet, cache_file, existing_db=None, cleanin
         print("database changed. saving to {}".format(cache_file))
     return new_db
 
-
-def fetch_and_save_all_databases(config, database_access, existing_quotes=None, existing_birthdays=None):
-
-    # fetch the new quotes file and save it if it is different than what we already have in memory
-    quotes = fetch_and_save_database(database_access['quote_source'], database_access['quote_sheet'], config['cache_quote_file'], existing_db=existing_quotes, cleaning_func=quote_db.clean_quotes)
-
-    if config['enable_birthday_quotes']:
-        # fetch the new birthdays file and save it if it is different than what we already have in memory
-        birthdays = fetch_and_save_database(database_access['quote_source'], database_access['birthday_sheet'], config['cache_birthday_file'], existing_db=existing_birthdays, cleaning_func=quote_db.clean_birthdays)
-
-    return quotes, birthdays
-
-
 def load_or_fetch_database(source, sheet, cache_file, cleaning_func):
-
+    # generic load or fetch function for both quotes and birthday information
     if not os.path.isfile(cache_file):
         print('fetching database from internet and saving to {}'.format(cache_file))
         df = fetch_and_save_database(source, sheet, cache_file, cleaning_func=cleaning_func)
@@ -69,18 +30,52 @@ def load_or_fetch_database(source, sheet, cache_file, cleaning_func):
         df = cleaning_func(df)
     return df
 
+class QuoteServerBackend:
+    def __init__(self, config, database) -> None:
+        # Load in dicts
+        self.config = config
+        self.database = database
 
-def load_or_fetch_all_databases(config, database_access):
-    quotes = load_or_fetch_database(database_access['quote_source'], database_access['quote_sheet'], config['cache_quote_file'], quote_db.clean_quotes)
+        # Set up data structures
+        self.quotes=pd.DataFrame()
+        self.birthdays=pd.DataFrame()
+        self.current_quote_index=0
+        self.recent_quotes = []
 
-    birthdays = None
-    if config['enable_birthday_quotes']:
-        birthdays = load_or_fetch_database(database_access['quote_source'], database_access['birthday_sheet'], config['cache_birthday_file'], quote_db.clean_birthdays)
+        # Get the quotes
+        self.load_or_fetch_all_databases()
 
-    return quotes, birthdays
+    def get_current_quote(self):
 
+        n_quotes = len(self.quotes)
 
-def on_post(self, config, config_file, quotes, birthdays):
+        chosen_quote = None
+
+        # safety check db size
+        if n_quotes > 0:
+            chosen_quote = self.quotes.iloc[self.current_quote_index % n_quotes].to_dict()
+
+        return chosen_quote
+
+    def change_current_quote(self):
+        self.current_quote_index = self.current_quote_index+1
+
+    def fetch_and_save_all_databases(self):
+
+        # fetch the new quotes file and save it if it is different than what we already have in memory
+        self.quotes = fetch_and_save_database(self.database['quote_source'], self.database['quote_sheet'], self.config['cache_quote_file'], existing_db=self.quotes, cleaning_func=quote_db.clean_quotes)
+
+        if 'birthday_sheet' in self.database and self.database['birthday_sheet']:
+            # fetch the new birthdays file and save it if it is different than what we already have in memory
+            self.birthdays = fetch_and_save_database(self.database['quote_source'], self.database['birthday_sheet'], self.config['cache_birthday_file'], existing_db=self.birthdays, cleaning_func=quote_db.clean_birthdays)
+
+    def load_or_fetch_all_databases(self):
+        self.quotes = load_or_fetch_database(self.database['quote_source'], self.database['quote_sheet'], self.config['cache_quote_file'], quote_db.clean_quotes)
+        
+        if 'birthday_sheet' in self.database and self.database['birthday_sheet']:
+            self.birthdays = load_or_fetch_database(self.database['quote_source'], self.database['birthday_sheet'], self.config['cache_birthday_file'], quote_db.clean_birthdays)
+
+def on_post(self, backend, config_file):
     # get the json sent to us
     content_len = int(self.headers.get('Content-Length'))
     post_body = self.rfile.read(content_len)
@@ -89,22 +84,25 @@ def on_post(self, config, config_file, quotes, birthdays):
 
     # parse the incoming dict for commands
     if 'force_quote_change' in incoming_dict and incoming_dict['force_quote_change']:
-        change_current_quote()
+        backend.change_current_quote()
+
+    if 'force_db_update' in incoming_dict and incoming_dict['force_db_update']:
+        backend.fetch_and_save_all_databases()
 
 
     # parse the incoming dict for settings
-    merge_dict_into_dict(config, incoming_dict, save_file=config_file)
+    merge_dict_into_dict(backend.config, incoming_dict, save_file=config_file)
     
     # build our response dict
     response_dict = {}
 
     # append current quote info
-    chosen_quote = get_current_quote(quotes)
+    chosen_quote = backend.get_current_quote()
     if chosen_quote is not None:
         response_dict['current_quote_info'] = dict_keys_to_lowercase(chosen_quote)
 
     # append settings
-    response_dict['config'] = dict_keys_to_lowercase(config)
+    response_dict['config'] = dict_keys_to_lowercase(backend.config)
 
     # send response
     self.send_response(200)
@@ -142,17 +140,14 @@ def main(database_access_file: str, config_file: str):
         merge_dict_into_dict(config, json.load(f), save_file=config_file)
 
     # load or fetch databaseses and save to csv if not present
+    backend = QuoteServerBackend(config=config, database=database_access)
 
-    quotes = pd.DataFrame()
-    birthdays = pd.DataFrame()
-    quotes, birthdays = load_or_fetch_all_databases(config, database_access)
     print("quotes:")
-    print(quotes)
+    print(backend.quotes)
     print()
     print("birthdays:")
-    print(birthdays)
+    print(backend.birthdays)
     print()
-
 
     # start time 
     current_time_s = time.time()
@@ -160,9 +155,11 @@ def main(database_access_file: str, config_file: str):
     last_quote_change_s = current_time_s
 
     handler = partial(HTTPHandler, 
-                      on_post=partial(lambda self: on_post(self, config, config_file, quotes, birthdays)), # bind additional arguments to our on_post callback
-                      root_dir='root', 
-                      default_index="index.html")
+                    on_post=partial(lambda self: on_post(self, backend, config_file)), # bind additional arguments to our on_post callback
+                    root_dir='root', 
+                    default_index="index.html")
+    
+
     server = HTTPServer(('', config['port']), handler) # access with http://localhost:8080
 
     # server.serve_forever()
@@ -171,24 +168,19 @@ def main(database_access_file: str, config_file: str):
         # THIS IS BLOCKING
         server.handle_request()
 
-        #
-        # there is no strong reason to multithread this. 
-        # if the website isnt requesting a quote, there's no need to maintain the databases
-        #
-
         current_time_s = time.time()
 
         # check if we need to update the quote db
-        next_db_query_s = last_db_query_s + config['database_query_period_m']*60 # written this way because updates rates may change
+        next_db_query_s = last_db_query_s + backend.config['database_query_period_m']*60 # written this way because updates rates may change
         if current_time_s >= next_db_query_s:
             last_db_query_s = current_time_s # allows slipping but thats fine for us
             print('updating database')
-            quotes, birthdays = fetch_and_save_all_databases(config, database_access, existing_quotes=quotes, existing_birthdays=birthdays)
+            backend.fetch_and_save_all_databases()
 
-        next_quote_change_s = last_quote_change_s + config['quote_change_period_m']*60 # written this way because updates rates may change
-        if current_time_s >= next_quote_change_s:
+        next_quote_change_s = last_quote_change_s + backend.config['quote_change_period_m']*60 # written this way because updates rates may change
+        if current_time_s >= next_quote_change_s and not ('sfw' in backend.config and backend.config['sfw']): # Prevents updating while not displaying quotes
             last_quote_change_s = current_time_s # allows slipping but thats fine for us
-            change_current_quote()
+            backend.change_current_quote()
 
 
 if __name__ == "__main__":
